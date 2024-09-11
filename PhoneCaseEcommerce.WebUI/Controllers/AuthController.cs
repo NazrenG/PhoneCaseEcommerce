9dd08812dce6b41b0394779f403f2c343c4bae9f
Sevgi <sevgi.elesgerova@gmail.com>
2024-09-11 17:20:12 +0400

    Register

---
+++ PhoneCaseEcommerce.WebUI/Controllers/AuthController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PhoneCaseEcommerce.DataAccess.Abstract;
+using PhoneCaseEcommerce.Entities.Models;
+using PhoneCaseEcommerce.WebUI.Models;
+
+namespace PhoneCaseEcommerce.WebUI.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthDal _authDal;
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IAuthDal dal, IConfiguration configuration)
+        {
+            _authDal = dal;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel vm)
+        {
+            var user = new User
+            {
+                Fullname = vm.Username,
+                PhoneNumber = vm.Phone,
+                Email = vm.Email,
+
+            };
+           await _authDal.Register(user, vm.Password);
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            //var user = new User
+            //{
+            //    Fullname = vm.Username,
+            //    PhoneNumber = vm.Phone,
+            //    Email = vm.Email,
+
+            //};
+            //await _authDal.Register(user, vm.Password);
+            //return RedirectToAction("Index", "Home");
+            return View();
+        }
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+    }
+}
---
+++ PhoneCaseEcommerce.WebUI/Models/RegisterViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Identity.Client;
+using PhoneCaseEcommerce.Entities.Models;
+
+namespace PhoneCaseEcommerce.WebUI.Models
+{
+    public class RegisterViewModel
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Phone {  get; set; }
+        public string Password { get; set; }
+    }
+}
